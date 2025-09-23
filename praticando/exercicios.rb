7e6530e4ee199c37bef0698ae2311421cc3ce43a
mariocesarfilho <mariocesarmoliveirafilho@gmail.com>
2025-09-23 14:57:28 -0300

    Exercise to transform all letters into capital letters

--- praticando/exercicios.rb
+++ praticando/exercicios.rb
@@ -1,16 +1,25 @@
-# -------Exercícios-------
+# # -------Exercícios-------
 
-# 1.  Crie um array de números e imprima apenas os números pares usando
-#     `select`.
+# # 1.  Crie um array de números e imprima apenas os números pares usando
+# #     `select`.
 
-number = [1,2,3,4,5,6,7,8,9,10] 
+# number = [1,2,3,4,5,6,7,8,9,10] 
 
-# Utilizando select
+# # Utilizando select
 
-puts number.select {|number| number%2==0}
+# puts number.select {|number| number%2==0}
 
-# for i in v
-#     if i%2==0
-#         puts "#{i} é um número par"
-#     end
-# end
+# Utilizando laço de repetição
+
+# # for i in v
+# #     if i%2==0
+# #         puts "#{i} é um número par"
+# #     end
+# # end
+
+
+# 2.  Dado um array de strings, transforme todas em maiúsculas com `map`.
+
+words = ['carro', 'maça','porta', 'goiaba']
+
+puts words.map {|words| words.upcase}
