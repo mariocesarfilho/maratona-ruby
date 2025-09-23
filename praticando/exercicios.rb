8ee35000a933faa35dfa274ce8a5fe06d956bf78
mariocesarfilho <mariocesarmoliveirafilho@gmail.com>
2025-09-23 14:45:25 -0300

    Add exercicios.rb with initial exercise for selecting even numbers

---
+++ praticando/exercicios.rb
@@ -0,0 +1,16 @@
+# -------Exercícios-------
+
+# 1.  Crie um array de números e imprima apenas os números pares usando
+#     `select`.
+
+number = [1,2,3,4,5,6,7,8,9,10] 
+
+# Utilizando select
+
+puts number.select {|number| number%2==0}
+
+# for i in v
+#     if i%2==0
+#         puts "#{i} é um número par"
+#     end
+# end
