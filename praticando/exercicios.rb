fed5cdba80be4de945aed0ddcd166a1620865924
mariocesarfilho <mariocesarmoliveirafilho@gmail.com>
2025-09-24 11:08:16 -0300

    Add exercise to create a hash of books and display title and author

--- praticando/exercicios.rb
+++ praticando/exercicios.rb
@@ -144,3 +144,17 @@ require 'debug'
 # end
 
 # testnumber(number)
+
+
+# 7.  Crie um hash de livros com título e autor, e itere exibindo
+#     `"Título - Autor"`.
+
+books = [
+    { titulo: "A Lenda", autor: "Smith Valder" },
+    { titulo: "Guerra do Infinito", autor: "Gordon Swthimeths" },
+    { titulo: "Monstros SA", autor: "Disney"}
+]
+
+books.each do |livros|
+    puts " #{livros[:titulo]} - #{livros[:autor]} "
+end
