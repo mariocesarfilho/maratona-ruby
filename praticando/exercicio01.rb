94bb4f223c7edd50566a2216c51ad5c724054197
mariocesarfilho <mariocesarmoliveirafilho@gmail.com>
2025-09-24 17:29:02 -0300

    Add exercise to print even numbers from an array using select and loop

---
+++ praticando/exercicio01.rb
@@ -0,0 +1,16 @@
+# # 1.  Crie um array de números e imprima apenas os números pares usando
+# #     `select`.
+
+number = [1,2,3,4,5,6,7,8,9,10] 
+
+# Utilizando select
+
+puts number.select {|number| number%2==0}
+
+Utilizando laço de repetição
+
+for i in v
+    if i%2==0
+        puts "#{i} é um número par"
+    end
+end
