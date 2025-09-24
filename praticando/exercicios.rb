879a5355616e142fb0bc5d69d0e660ec256264f4
mariocesarfilho <mariocesarmoliveirafilho@gmail.com>
2025-09-24 16:04:40 -0300

    Add exercise to count letter frequency in a given phrase

--- praticando/exercicios.rb
+++ praticando/exercicios.rb
@@ -234,3 +234,13 @@ require 'debug'
 
 # print other
 
+# 12. Dada a frase `"ruby on rails é incrível"`, conte a frequência de
+#     cada letra.
+
+frase = "ruby on rails é incrível"
+cnt = 0 #Guardar a quantidade de vezes que o caracter aparece
+
+for char in frase.chars.uniq
+    cnt = frase.count(char)
+    puts "O caracter #{char} apareceu #{cnt} na frase"
+end
