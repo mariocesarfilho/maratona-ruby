339fc691605a3967646600fb61abf6174997111e
mariocesarfilho <mariocesarmoliveirafilho@gmail.com>
2025-09-25 11:06:20 -0300

    bug, ainda não terminado exercicio de soma com recursividade

--- praticando/exerciciorecursividade.rb
+++ praticando/exerciciorecursividade.rb
@@ -1,3 +1,5 @@
+require 'debug'
+puts Gem.loaded_specs["debug"].version
 # Imprimir de 1 até 10 com recursividade
 
 def numberrecursive(count = 1)
@@ -22,4 +24,29 @@ def fatorial(number = 5)
     
 end
 
-puts "Resultado Final #{fatorial}"
+# puts "Resultado Final #{fatorial}"
+
+
+
+# 1. Implemente um método recursivo que receba como entrada um número inteiro
+# positivo N e retorne o seguinte cálculo:
+
+puts "Digite um Número Inteiro: "
+number = gets.chomp.to_i
+
+def soma(counter=1, number)
+    puts " Somando: #{counter}"
+
+    if number == 0
+
+        puts "Número inválido"
+    else
+    #    binding.break 
+        if counter < number
+            soma(counter + 1)
+        end
+    end
+end
+
+
+soma
